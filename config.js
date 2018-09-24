0ff59e47dde3c5d9d48b529aceda808b60f04e27
Galiant <antonijodev@gmail.com>
2018-09-24 14:33:20 +0100

    Created config.js file

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export default {
     title: 'React Universal Blog'
   },
   bucket: {
-    slug: process.env.COSMIC_BUCKET || '9d6b5dd0-bfe7-11e8-9939-05b55120fcc3',
+    slug: process.env.COSMIC_BUCKET || 'antonijos-react-blog',
     media_url: 'https://cosmicjs.com/uploads',
     read_key: process.env.COSMIC_READ_KEY || '',
     write_key: process.env.COSMIC_WRITE_KEY || ''
